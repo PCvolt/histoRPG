058b9302c9e980f32bf286166ba8ef831817feb3
PCvolt <31967641+PCvolt@users.noreply.github.com>
2017-09-29 17:45:48 +0200

    Add files via upload

---
+++ mana/config.h
@@ -0,0 +1,15 @@
+#define CONF_APP_NAME           "Animations SDL2"
+
+#define CONF_MAP_X              20
+#define CONF_MAP_Y              20
+#define CONF_MAP_TILESIZE       15
+
+
+#define CONF_WINW	        CONF_MAP_X * CONF_MAP_TILESIZE
+#define CONF_WINH               CONF_MAP_Y * CONF_MAP_TILESIZE
+#define CONF_WINPOS             SDL_WINDOWPOS_UNDEFINED
+
+#define CONF_PIXELFORMAT	SDL_PIXELFORMAT_RGBA8888
+#define CONF_CLEAR_COLOR	0, 5, 25
+
+#define CONF_KEY_EXIT		SDL_SCANCODE_Q
